c5e3d25665c15a648e084cbbabb20e9114656c33
yasminnelm <yassamineelm11@gmail.com>
2025-01-03 13:41:15 +0100

    Initial navbar design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {RegistrationFormComponent} from "./components/regisitration-form/regisitration-form.component";
 import {EmailConfirmationComponent} from "./components/email-confirmation/email-confirmation.component";
 import {PhoneConfirmationComponent} from "./components/phone-confirmation/phone-confirmation.component";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
 export const routes: Routes = [
   {
@@ -9,7 +10,8 @@ export const routes: Routes = [
     children: [
       { path: 'register', component: RegistrationFormComponent }, // Child path
       { path: 'email-confirmation', component: EmailConfirmationComponent }, // Child path for email confirmation
-      {path : 'phone-confirmation' ,  component : PhoneConfirmationComponent}
+      {path : 'phone-confirmation' ,  component : PhoneConfirmationComponent},
+      {path: 'homepage', component: LandingPageComponent}
     ]
   }
 ];
