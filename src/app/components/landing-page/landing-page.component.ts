21d616a07d62531964791c293d795ae501f0e4db
yasminnelm <yassamineelm11@gmail.com>
2025-01-04 15:02:52 +0100

    slider div

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {SliderComponent} from "../slider/slider.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    SliderComponent
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+  currentSlide = 0; // Tracks the current slide index
+  totalSlides = 4; // Update based on the number of slides
 
+  // Go to the previous slide
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.totalSlides) % this.totalSlides;
+    this.updateSliderPosition();
+  }
+
+  // Go to the next slide
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+    this.updateSliderPosition();
+  }
+
+  // Update the slider's transform property
+  updateSliderPosition() {
+    const slider = document.querySelector('.slider') as HTMLElement;
+    slider.style.transform = `translateX(-${this.currentSlide * 100}%)`;
+  }
 }
