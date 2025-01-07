523f97fb11edf6b810dd3228da50d1815f9e47cb
yasminnelm <yassamineelm11@gmail.com>
2025-01-07 12:24:43 +0100

    sidebar fix

---
+++ src/app/components/creance/creance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creance',
+  standalone: true,
+  imports: [],
+  templateUrl: './creance.component.html',
+  styleUrl: './creance.component.css'
+})
+export class CreanceComponent {
+
+}
