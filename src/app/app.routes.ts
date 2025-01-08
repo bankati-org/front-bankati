b5e766d8c77f8f9e6d37cbc72daa8db57b5fae29
Soukaina Jabri <soukaina.jabri333@gmail.com>
2025-01-08 17:36:05 +0100

    Change path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,7 +17,7 @@ import {CreanceComponent} from "./components/creance/creance.component";
 
 export const routes: Routes = [
   {
-    path: 'app', // Parent path
+    path: '', // Parent path
     children: [
       { path: 'register', component: RegistrationFormComponent }, // Child path
       { path: 'email-confirmation', component: EmailConfirmationComponent }, // Child path for email confirmation
