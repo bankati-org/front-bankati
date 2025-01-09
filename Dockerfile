55bd0f7e9c71340a8e43cf769bbf985139d96bd4
Soukaina Jabri <soukaina.jabri333@gmail.com>
2025-01-09 01:50:42 +0100

    file replacements

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN npm install
 
 COPY . .
 
-RUN npm run build --prod
+RUN npm run ng build -- --configuration=production
 
 FROM nginx:alpine
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   apiUrl: 'http://34.41.170.207:30107/',
-  // walletApiUrl: 'http://34.41.170.207:30107/'
 };
